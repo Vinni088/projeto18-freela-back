e4fa54a63022f0020092577364d90cc69951a974
Vinicius Evangelista <viniciusevangelista088@gmail.comgit config --global user.name Vinni>
2023-12-10 21:11:22 -0300

    feat: demo rota "/"

--- src/app.js
+++ src/app.js
@@ -7,6 +7,30 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(router)
+app.get("/", (req, res) => {
+    res.status(200).send(
+      `
+      <div style="
+        width: 95vw; 
+        height: 95vh; 
+        display: flex; 
+        justify-content: center; 
+        align-items: center;
+      ">
+      <p style="
+        padding: 10px;
+        font-size: 2vmax; 
+        border: 2px solid gray;
+        background-color: lightgray;
+        border-radius: 10px;
+      ">
+        Opa! parece que você está tentando acessar o backend do "GetSamurais" <br/>
+        Se esse for o caso, leia o ReadMe do projeto em: <a href="https://github.com/Vinni088/projeto18-freela-back"> Github: projeto18-freela-back </a> 
+      </p>
+    <div>
+      `
+    );
+  });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Running server on port ${PORT}`));
